a3bc218afa79066a4bcae4b7459c667a7ddf106e
Devin1824 <devster18@gmail.com>
2022-12-13 09:48:59 -0800

    initial commit

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter a temperature in Farenheit to be converted to Celsius: ");
+            double temp = double.Parse(Console.ReadLine());
+            double answer = TempConverter.FarenheitToCelsius(temp);
+            Console.WriteLine($"{temp} Farenheit converted to Celsius is {answer}");
+
+            Console.WriteLine("Enter a temperature in Celsius to be converted to Farenheit: ");
+            double newTemp = double.Parse(Console.ReadLine());
+            double answer2 = TempConverter.CelsiusToFarenheit(newTemp);
+            Console.WriteLine($"{newTemp} Celsius converted to Farenheit is {answer2}");
+
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FarenheitToCelsius(double number)
+        {
+            double celsius = (number - 32) * 5 / 9; 
+            return celsius;
+        }
+
+        public static double CelsiusToFarenheit(double number)
+        {
+            double far = (number * 9 / 5) + 32;
+            return far;
+        }
+    }
+}
